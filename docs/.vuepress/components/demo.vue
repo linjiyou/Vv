3dacce093dc562e3a2c54f580af38aa9141c9bd8
@llinjiyoull <2661706455@qq.com>
2020-09-28 10:22:24 +0800

    'init'

---
+++ docs/.vuepress/components/demo.vue
@@ -0,0 +1,13 @@
+<template>
+ 
+ <h1><g-Button></g-Button></h1>
+</template>
+<script>
+  import Button from '../../../src/button'
+  export default{
+      name:"demo",
+      components:{
+          gButton:Button
+      }
+  }
+</script>
---
+++ src/button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button >
+      ddd
+       <span>heee</span>
+  </button>
+</template>
+<script>
+ 
+  export default {
+    name: 'GuluButton',
+    
+  }
+</script>
+<style lang="less" scoped>
+  button{
+      font-size: 15px;
+      span{
+          display: block;
+
+      }
+  }
+  
+</style>
